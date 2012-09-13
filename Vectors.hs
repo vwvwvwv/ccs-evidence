483bf31ba4007b575dca38a5b2acaba400e002a6
Miles Bosson <milesbosson@gmail.com>
2012-09-12 21:27:05 -0700

    some minor changes and some broken, commented out code

--- Vectors.hs
+++ Vectors.hs
@@ -1,5 +1,7 @@
 module Vectors where
 
+import System.Random (randomRIO)
+
 data Vector a = Vect a a a
               deriving (Show, Eq)
 
@@ -8,22 +10,30 @@ foldVect1 op = foldr1 applyOp
   where
     applyOp (Vect x0 y0 z0) (Vect x1 y1 z1) =
       Vect (op x0 x1) (op y0 y1) (op z0 z1)
-
+    
 mapVect    :: (Num a) => (a -> a) -> [Vector a] -> [Vector a]
 mapVect op = map applyOp
   where
     applyOp (Vect x y z) = Vect (op x) (op y) (op z)
-
+    
 sumVectors = foldVect1 (+)
-
+             
 subtractVectors :: [Vector Double] -> Vector Double
 subtractVectors = foldVect1 (-)
-
+                  
 dotVectors :: (Num a) => [Vector a] -> a
 dotVectors = dot . foldVect1 (*)
   where
     dot :: (Num a) => Vector a -> a
     dot (Vect x y z) = x+y+z
-
+    
 scaleVectors s = mapVect (* s)
-
+                 
+{- Need to make this work!
+getRandomVect :: Double -> Vector (IO Double)
+getRandomVect r = Vect x y z
+  where
+    x = randomRIO (0.0, r)
+    y = randomRIO (0.0, r)
+    z = randomRIO (0.0, r)
+-}
