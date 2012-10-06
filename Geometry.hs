dbcdc4576e25848132cb81b53998e8010d124bb2
Miles Bosson <milesbosson@gmail.com>
2012-10-06 14:28:30 -0700

    Added a `Geometry' library, containing code for vectors, angles, and torsional angles. Planning to have a clean start with the rest of the project.

---
+++ Geometry.hs
@@ -0,0 +1,56 @@
+module Geometry where
+
+data Vector = Vector Double Double Double
+            deriving (Show, Eq)
+
+data Angle = Angle Vector Vector Vector
+            deriving (Show, Eq)
+
+data TorsAngle = TorsAngle Vector Vector Vector Vector
+            deriving (Show, Eq)
+
+vector0 :: Vector
+vector0 = Vector 0 0 0
+
+wrapVector :: (Double -> Double -> Double) -> Vector -> Vector -> Vector
+wrapVector f (Vector x x' x'') (Vector y y' y'') = 
+    Vector (f x y) (f x' y') (f x'' y'')
+
+(@+) :: Vector -> Vector -> Vector 
+(@+) = wrapVector (+)
+
+(@-) :: Vector -> Vector -> Vector 
+(@-) = wrapVector (-)
+
+(@*) :: Vector -> Vector -> Vector 
+(@*) = wrapVector (*)
+
+(@/) :: Vector -> Vector -> Vector
+(@/) = wrapVector (/)
+
+(@.) :: Vector -> Vector -> Double
+(@.) (Vector x x' x'') (Vector y y' y'') = x*y + x'*y' + x''*y''
+
+(@#) :: Vector -> Vector -> Vector
+(@#) (Vector x x' x'') (Vector y y' y'') = 
+    Vector (x'*y''-x''*y') (x''*y-x*y'') (x*y'-x'*y)
+
+distance :: Vector -> Vector -> Double
+distance v v' = sqrt $ v @. v'
+
+magnitude :: Vector -> Double
+magnitude v = distance v v
+
+measureAngle :: Angle -> Double
+measureAngle (Angle v v' v'') =
+    asin $ (magnitude (a @# b)) / ((magnitude a) * (magnitude b))
+  where
+    a = v @- v'
+    b = v'' @- v'
+
+measureTorsAngle :: TorsAngle -> Double
+measureTorsAngle (TorsAngle v v' v'' v''') = 
+    asin $ (magnitude (a @# b)) / ((magnitude a) * (magnitude b))
+  where
+    a = (v'@-v) @# (v''@-v)
+    b = (v'@-v''') @# (v''@-v''')
