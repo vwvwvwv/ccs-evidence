0c319c9514e9c3a664078c38dced28d1342aa659
Miles Bosson <milesbosson@gmail.com>
2012-10-06 23:46:26 -0700

    began work on a visualizer

--- Main.hs
+++ Main.hs
@@ -36,14 +36,17 @@ main = do
     putStrLn (show v)
     putStrLn (show v')
   where
-    atoms = [ Atom {pos=Vector 0 0 0,charge=0,radius=1,atomId=1}
-            , Atom {pos=Vector 1 0 0,charge=0,radius=1,atomId=2}
-            , Atom {pos=Vector 2 2 2,charge=0,radius=1,atomId=3}
-            , Atom {pos=Vector 5 5 3,charge=1,radius=1.5,atomId=4}
-            ]
-    bonds = [ Bond (atoms!!0) (atoms!!1)
-            , Bond (atoms!!0) (atoms!!2)
-            ]
     bondAngles = collectBondAngles bonds
     bondTorsAngles = collectBondTorsAngles bondAngles
     ms = (atoms,bonds,bondAngles,bondTorsAngles)
+
+atoms = [ Atom {pos=Vector 0 0 0,charge=0,radius=1,atomId=1}
+        , Atom {pos=Vector 1 0 0,charge=0,radius=1,atomId=2}
+        , Atom {pos=Vector 2 2 2,charge=0,radius=1,atomId=3}
+        , Atom {pos=Vector 5 5 3,charge=1,radius=1.5,atomId=4}
+        ]
+
+bonds = [ Bond (atoms!!0) (atoms!!1)
+        , Bond (atoms!!0) (atoms!!2)
+        ]
+
